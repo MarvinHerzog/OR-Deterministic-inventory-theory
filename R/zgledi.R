ade49c371c84edc20198729311c3eacd32664b78
MarvinHerzog <marvin.herzog@hotmail.com>
2017-02-22 17:46:22 +0100

    Zadnji popravki, dodano diskontiranje, dodaten zgled

--- R/zgledi.R
+++ R/zgledi.R
@@ -7,6 +7,9 @@ L = 2   #lead time (in unit-time)
 D = 150     # Pricakovana vrednost povprasevanja na enoto casa
 sd1 = 0 #Standardni odklon povprasevanja na enoto casa
 p = 0.10 #shortage cost (ce smo pod y=0)
+c1 = 0.1 #Normalna cena
+c2 = 0.06 #cena èe je naroèilo nad q
+q = 1500 #q meja kjer dobimo popust na naroèilo (se uveljavlja c2)
 B = 0 #buffer, tj. varnostni dodatek k RP. Za zeljeno verjetnost, da pademo pod y=0, uporabi formulo B=VaR - Le *D (VaR value at risk za dano porazdelitev)
 RP.force = NA #sami izberemo reorder point (sicer nastavi na NA)
 yopt.force = NA #sami izberemo kolicino, ki jo narocimo (sicer nastavi na NA)
@@ -16,6 +19,7 @@ stkor = 250 #zgornjih tock je stkor mnogo
 seme = runif(1,0,1000)
 #seme = 18
 
+diskontiranje = F #ali vkljucimo popust ob nakupu vecje kolicine dobrine
 allowshort = F  #dopuscamo nacrtovano pomanjkanje dobrin
 prepreci.propad1 = F #ce mine ves cikel, brez da preckamo RP, naroci na koncu cikla
 prepreci.propad2 = F #ce smo po skoku se vedno pod RP, naroci taksno narocilo, da bo ob upostevanju vseh prihodnih skokov vrednost zalog na S
@@ -25,6 +29,39 @@ t0.namesto.RP = F #naroci periodicno z t0 namesto po preckanju RP
 TCtest = F #total cost test, nastavi na F ce noces testirat, sicer izberi parameter ki ga hoces perturbirat (npr "RP") . V if-u spodaj spremeni korak in st. korakov
 DP = F #Simulacija rezultata dobljenega z dinamicnim programiranjem, PAZI: izklopi propad2 ter allowshort!
 pikice = T
+
+#Razlika v TC brez/z diskontiranjem
+n = 30 #st obdobij
+K = 50  #setup cost (per order)
+h = 0.05 #holding cost (per inventory-unit per unit-time)
+L = 2   #lead time (in unit-time)
+D = 150     # Pricakovana vrednost povprasevanja na enoto casa
+sd1 = 0 #Standardni odklon povprasevanja na enoto casa
+p = 0.10 #shortage cost (ce smo pod y=0)
+c1 = 0.1 #Normalna cena
+c2 = 0.06 #cena ce je narocilo nad q
+q = 800 #q meja kjer dobimo popust na narocilo (se uveljavlja c2)
+B = 0 #buffer, tj. varnostni dodatek k RP. Za zeljeno verjetnost, da pademo pod y=0, uporabi formulo B=VaR - Le *D (VaR value at risk za dano porazdelitev)
+RP.force = NA #sami izberemo reorder point (sicer nastavi na NA)
+yopt.force = NA #sami izberemo kolicino, ki jo narocimo (sicer nastavi na NA)
+r = NA #ce zelis vstaviti svoj vektor povprasevanj, sicer pusti na NA (po default vzame rep(D,n) )
+korak = 0.005 # inkrement v zanki za testiranje stroskov, ce je korak npr 0.005, potem nastavimo zeljeno spremenljivko na 100%, 99.5%, 100.5%, 99%, 101% itd.
+stkor = 80 #zgornjih tock je stkor mnogo
+seme = runif(1,0,1000)
+#seme = 18
+
+diskontiranje = F #ali vkljucimo popust ob nakupu vecje kolicine dobrine
+allowshort = F  #dopuscamo nacrtovano pomanjkanje dobrin
+prepreci.propad1 = F #ce mine ves cikel, brez da preckamo RP, naroci na koncu cikla
+prepreci.propad2 = F #ce smo po skoku se vedno pod RP, naroci taksno narocilo, da bo ob upostevanju vseh prihodnih skokov vrednost zalog na S
+prednarocila = T #ce L>Le, prvih nekaj obdobij naroci ob fiksnih cas. intervalih (kot da je Y deterministicen)
+LE.namesto.L = F #uporabi efektivni lead time - lepsi rezultati ob nestabilnih Y, ni pa realisticno
+t0.namesto.RP = F #naroci periodicno z t0 namesto po preckanju RP
+TCtest = F #total cost test, nastavi na F ce noces testirat, sicer izberi parameter ki ga hoces perturbirat (npr "RP") . V if-u spodaj spremeni korak in st. korakov
+DP = F #Simulacija rezultata dobljenega z dinamicnim programiranjem, PAZI: izklopi propad2 ter allowshort!
+pikice = T
+
+
 
 #http://imgur.com/a/m4uy5
 #demonstracija DP, primer iz literature
@@ -36,6 +73,9 @@ L = 2   #lead time (in unit-time)
 D = 150     # Pricakovana vrednost povprasevanja na enoto casa
 sd1 = 0 #Standardni odklon povprasevanja na enoto casa
 p = 0.10 #shortage cost (ce smo pod y=0)
+c1 = 0.1 #Normalna cena
+c2 = 0.06 #cena èe je naroèilo nad q
+q = 1500 #q meja kjer dobimo popust na naroèilo (se uveljavlja c2)
 B = 0 #buffer, tj. varnostni dodatek k RP. Za zeljeno verjetnost, da pademo pod y=0, uporabi formulo B=VaR - Le *D (VaR value at risk za dano porazdelitev)
 RP.force = NA #sami izberemo reorder point (sicer nastavi na NA)
 yopt.force = NA #sami izberemo kolicino, ki jo narocimo (sicer nastavi na NA)
@@ -46,6 +86,7 @@ stkor = 250 #zgornjih tock je stkor mnogo
 seme = runif(1,0,1000)
 #seme = 55
 
+diskontiranje = F
 allowshort = F  #dopuscamo nacrtovano pomanjkanje dobrin
 prepreci.propad1 = F #ce mine ves cikel, brez da preckamo RP, naroci na koncu cikla
 prepreci.propad2 = F #ce smo po skoku se vedno pod RP, naroci taksno narocilo, da bo ob upostevanju vseh prihodnih skokov vrednost zalog na S
@@ -67,6 +108,9 @@ L = 2   #lead time (in unit-time)
 D = 150     # Pricakovana vrednost povprasevanja na enoto casa
 sd1 = 50 #Standardni odklon povprasevanja na enoto casa
 p = 0.10 #shortage cost (ce smo pod y=0)
+c1 = 0.1 #Normalna cena
+c2 = 0.06 #cena èe je naroèilo nad q
+q = 1500 #q meja kjer dobimo popust na naroèilo (se uveljavlja c2)
 B = 0 #buffer, tj. varnostni dodatek k RP. Za zeljeno verjetnost, da pademo pod y=0, uporabi formulo B=VaR - Le *D (VaR value at risk za dano porazdelitev)
 RP.force = NA #sami izberemo reorder point (sicer nastavi na NA)
 yopt.force = NA #sami izberemo kolicino, ki jo narocimo (sicer nastavi na NA)
@@ -76,6 +120,7 @@ stkor = 250 #zgornjih tock je stkor mnogo
 #seme = runif(1,0,1000)
 seme = 55
 
+diskontiranje = F
 allowshort = T  #dopuscamo nacrtovano pomanjkanje dobrin
 prepreci.propad1 = F #ce mine ves cikel, brez da preckamo RP, naroci na koncu cikla
 prepreci.propad2 = F #ce smo po skoku se vedno pod RP, naroci taksno narocilo, da bo ob upostevanju vseh prihodnih skokov vrednost zalog na S
@@ -96,6 +141,9 @@ L = 13   #lead time (in unit-time)
 D = 150     # Pricakovana vrednost povprasevanja na enoto casa
 sd1 = 50 #Standardni odklon povprasevanja na enoto casa
 p = 0.10 #shortage cost (ce smo pod y=0)
+c1 = 0.1 #Normalna cena
+c2 = 0.06 #cena èe je naroèilo nad q
+q = 1500 #q meja kjer dobimo popust na naroèilo (se uveljavlja c2)
 B = 0 #buffer, tj. varnostni dodatek k RP. Za zeljeno verjetnost, da pademo pod y=0, uporabi formulo B=VaR - Le *D (VaR value at risk za dano porazdelitev)
 RP.force = NA #sami izberemo reorder point (sicer nastavi na NA)
 yopt.force = NA #sami izberemo kolicino, ki jo narocimo (sicer nastavi na NA)
@@ -105,6 +153,7 @@ stkor = 250 #zgornjih tock je stkor mnogo
 #seme = runif(1,0,1000)
 seme = 56
 
+diskontiranje = F
 allowshort = T  #dopuscamo nacrtovano pomanjkanje dobrin
 prepreci.propad1 = F #ce mine ves cikel, brez da preckamo RP, naroci na koncu cikla
 prepreci.propad2 = F #ce smo po skoku se vedno pod RP, naroci taksno narocilo, da bo ob upostevanju vseh prihodnih skokov vrednost zalog na S
@@ -127,6 +176,9 @@ L = 13   #lead time (in unit-time)
 D = 150     # Pricakovana vrednost povprasevanja na enoto casa
 sd1 = 70 #Standardni odklon povprasevanja na enoto casa
 p = 0.10 #shortage cost (ce smo pod y=0)
+c1 = 0.1 #Normalna cena
+c2 = 0.06 #cena èe je naroèilo nad q
+q = 1500 #q meja kjer dobimo popust na naroèilo (se uveljavlja c2)
 B = 0 #buffer, tj. varnostni dodatek k RP. Za zeljeno verjetnost, da pademo pod y=0, uporabi formulo B=VaR - Le *D (VaR value at risk za dano porazdelitev)
 RP.force = NA #sami izberemo reorder point (sicer nastavi na NA)
 yopt.force = NA #sami izberemo kolicino, ki jo narocimo (sicer nastavi na NA)
@@ -136,6 +188,7 @@ stkor = 250 #zgornjih tock je stkor mnogo
 #seme = runif(1,0,1000)
 seme = 16
 
+diskontiranje = F
 allowshort = T  #dopuscamo nacrtovano pomanjkanje dobrin
 prepreci.propad1 = F #ce mine ves cikel, brez da preckamo RP, naroci na koncu cikla
 prepreci.propad2 = F #ce smo po skoku se vedno pod RP, naroci taksno narocilo, da bo ob upostevanju vseh prihodnih skokov vrednost zalog na S
@@ -154,6 +207,9 @@ L = 13   #lead time (in unit-time)
 D = 150     # Pricakovana vrednost povprasevanja na enoto casa
 sd1 = 70 #Standardni odklon povprasevanja na enoto casa
 p = 0.10 #shortage cost (ce smo pod y=0)
+c1 = 0.1 #Normalna cena
+c2 = 0.06 #cena èe je naroèilo nad q
+q = 1500 #q meja kjer dobimo popust na naroèilo (se uveljavlja c2)
 B = 0 #buffer, tj. varnostni dodatek k RP. Za zeljeno verjetnost, da pademo pod y=0, uporabi formulo B=VaR - Le *D (VaR value at risk za dano porazdelitev)
 RP.force = NA #sami izberemo reorder point (sicer nastavi na NA)
 yopt.force = NA #sami izberemo kolicino, ki jo narocimo (sicer nastavi na NA)
@@ -163,6 +219,7 @@ stkor = 250 #zgornjih tock je stkor mnogo
 #seme = runif(1,0,1000)
 seme = 16
 
+diskontiranje = F
 allowshort = T  #dopuscamo nacrtovano pomanjkanje dobrin
 prepreci.propad1 = T #ce mine ves cikel, brez da preckamo RP, naroci na koncu cikla
 prepreci.propad2 = F #ce smo po skoku se vedno pod RP, naroci taksno narocilo, da bo ob upostevanju vseh prihodnih skokov vrednost zalog na S
@@ -186,6 +243,9 @@ L = 2   #lead time (in unit-time)
 D = 150     # Pricakovana vrednost povprasevanja na enoto casa
 sd1 = 120 #Standardni odklon povprasevanja na enoto casa
 p = 0.10 #shortage cost (ce smo pod y=0)
+c1 = 0.1 #Normalna cena
+c2 = 0.06 #cena èe je naroèilo nad q
+q = 1500 #q meja kjer dobimo popust na naroèilo (se uveljavlja c2)
 B = 0 #buffer, tj. varnostni dodatek k RP. Za zeljeno verjetnost, da pademo pod y=0, uporabi formulo B=VaR - Le *D (VaR value at risk za dano porazdelitev)
 RP.force = NA #sami izberemo reorder point (sicer nastavi na NA)
 yopt.force = NA #sami izberemo kolicino, ki jo narocimo (sicer nastavi na NA)
@@ -195,6 +255,7 @@ stkor = 250 #zgornjih tock je stkor mnogo
 #seme = runif(1,0,1000)
 seme = 39
 
+diskontiranje = F
 allowshort = T  #dopuscamo nacrtovano pomanjkanje dobrin
 prepreci.propad1 = F #ce mine ves cikel, brez da preckamo RP, naroci na koncu cikla
 prepreci.propad2 = F #ce smo po skoku se vedno pod RP, naroci taksno narocilo, da bo ob upostevanju vseh prihodnih skokov vrednost zalog na S
@@ -225,6 +286,9 @@ L = 2   #lead time (in unit-time)
 D = 150     # Pricakovana vrednost povprasevanja na enoto casa
 sd1 = 0 #Standardni odklon povprasevanja na enoto casa
 p = 0.10 #shortage cost (ce smo pod y=0)
+c1 = 0.1 #Normalna cena
+c2 = 0.06 #cena èe je naroèilo nad q
+q = 1500 #q meja kjer dobimo popust na naroèilo (se uveljavlja c2)
 B = 0 #buffer, tj. varnostni dodatek k RP. Za zeljeno verjetnost, da pademo pod y=0, uporabi formulo B=VaR - Le *D (VaR value at risk za dano porazdelitev)
 RP.force = NA #sami izberemo reorder point (sicer nastavi na NA)
 yopt.force = NA #sami izberemo kolicino, ki jo narocimo (sicer nastavi na NA)
@@ -234,6 +298,7 @@ stkor = 250 #zgornjih tock je stkor mnogo
 seme = runif(1,0,1000)
 #seme = 18
 
+diskontiranje = F
 allowshort = F  #dopuscamo nacrtovano pomanjkanje dobrin
 prepreci.propad1 = F #ce mine ves cikel, brez da preckamo RP, naroci na koncu cikla
 prepreci.propad2 = F #ce smo po skoku se vedno pod RP, naroci taksno narocilo, da bo ob upostevanju vseh prihodnih skokov vrednost zalog na S
@@ -254,6 +319,9 @@ L = 2   #lead time (in unit-time)
 D = 150     # Pricakovana vrednost povprasevanja na enoto casa
 sd1 = 0 #Standardni odklon povprasevanja na enoto casa
 p = 0.10 #shortage cost (ce smo pod y=0)
+c1 = 0.1 #Normalna cena
+c2 = 0.06 #cena èe je naroèilo nad q
+q = 1500 #q meja kjer dobimo popust na naroèilo (se uveljavlja c2)
 B = 0 #buffer, tj. varnostni dodatek k RP. Za zeljeno verjetnost, da pademo pod y=0, uporabi formulo B=VaR - Le *D (VaR value at risk za dano porazdelitev)
 RP.force = NA #sami izberemo reorder point (sicer nastavi na NA)
 yopt.force = NA #sami izberemo kolicino, ki jo narocimo (sicer nastavi na NA)
@@ -263,6 +331,7 @@ stkor = 190 #zgornjih tock je stkor mnogo
 #seme = runif(1,0,1000)
 seme = 20
 
+diskontiranje = F
 allowshort = T  #dopuscamo nacrtovano pomanjkanje dobrin
 prepreci.propad1 = F #ce mine ves cikel, brez da preckamo RP, naroci na koncu cikla
 prepreci.propad2 = T #ce smo po skoku se vedno pod RP, naroci taksno narocilo, da bo ob upostevanju vseh prihodnih skokov vrednost zalog na S
@@ -284,6 +353,9 @@ L = 2   #lead time (in unit-time)
 D = 150     # Pricakovana vrednost povprasevanja na enoto casa
 sd1 = 0 #Standardni odklon povprasevanja na enoto casa
 p = 0.10 #shortage cost (ce smo pod y=0)
+c1 = 0.1 #Normalna cena
+c2 = 0.06 #cena èe je naroèilo nad q
+q = 1500 #q meja kjer dobimo popust na naroèilo (se uveljavlja c2)
 B = 0 #buffer, tj. varnostni dodatek k RP. Za zeljeno verjetnost, da pademo pod y=0, uporabi formulo B=VaR - Le *D (VaR value at risk za dano porazdelitev)
 RP.force = NA #sami izberemo reorder point (sicer nastavi na NA)
 yopt.force = NA #sami izberemo kolicino, ki jo narocimo (sicer nastavi na NA)
@@ -293,6 +365,7 @@ stkor = 250 #zgornjih tock je stkor mnogo
 #seme = runif(1,0,1000)
 seme = 20
 
+diskontiranje = F
 allowshort = T  #dopuscamo nacrtovano pomanjkanje dobrin
 prepreci.propad1 = F #ce mine ves cikel, brez da preckamo RP, naroci na koncu cikla
 prepreci.propad2 = T #ce smo po skoku se vedno pod RP, naroci taksno narocilo, da bo ob upostevanju vseh prihodnih skokov vrednost zalog na S
